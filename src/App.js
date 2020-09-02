dc05c2c92a459367a3bc6f0f0f4b41ea2810ffa6
Prateek Kaushik <kaushikprateek11@gmail.com>
2020-09-02 11:14:54 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Home,
+  Dashboard,
+  Login,
+  NotFound,
+  Loading
+} from './pages/index'
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { GuardProvider, GuardedRoute } from 'react-router-guards';
+
+const requireLogin = (to, from, next) => {
+  if (to.meta.auth) {
+    if (true) {
+      next();
+    }
+    next.redirect('/login');
+  } else {
+    next();
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <GuardProvider guards={[requireLogin]} loading={Loading} error={NotFound}>
+          <Switch>
+            <GuardedRoute path="/login" exact component={Login} />
+            <GuardedRoute path="/" exact component={Home} meta={{ auth: true }} />
+            <GuardedRoute path="/dashboard" exact component={Dashboard} meta={{ auth: true }} />
+            <GuardedRoute path="*" component={NotFound} />
+          </Switch>
+        </GuardProvider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Home from './home'
+import Dashboard from './dashboard'
+import Login from './login'
+import NotFound from './notfound'
+import Loading from './loading'
+export {
+  Home,
+  Dashboard,
+  Login,
+  NotFound,
+  Loading
+}
